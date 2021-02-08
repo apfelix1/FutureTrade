885119e37ff81a2e59808c1e1876916f658642b0
apfelix1 <fanfei.002@gmail.com>
2021-02-08 08:55:59 +0800

    new project

--- TradeMethod.py
+++ TradeMethod.py
@@ -1,6 +1,8 @@
-#Start of new Trading algorithmwd
-class FutureTrade:
-    def __init__(self, ETF, date):
-        self.ETF_number = ETF
-        # yes
-        $no
+from higgsboom.MarketData.CFuturesMarketDataUtils import *
+
+fUtils = CFuturesMarketDataUtils('Z:/FuturesData', 'cffex-l2')
+
+fList = fUtils.FuturesTickDataFrame('IF2006', '20200103')
+print(fList)
+
+
