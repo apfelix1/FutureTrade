fd7902ecdfa64db118d8c36915d8deccf0022703
apfelix1 <fanfei.002@gmail.com>
2021-03-24 11:07:01 +0800

    new project

--- RealMoneySim.py
+++ RealMoneySim.py
@@ -6,12 +6,174 @@ import datetime
 
 fUtils = CFuturesMarketDataUtils('Z:/FuturesData', 'cffex-l2')
 secUtils = CSecurityMarketDataUtils('Z:/StockData')
+pd.set_option("display.max_columns", None)
 
-def get_sim_log(startDate, endDate, initamt, tradelmt = True):
+
+def get_sim_log(startDate, endDate, initamt):
+    balance = pd.DataFrame(columns = ['date', 'timetick', 'tradeMove', 'BalanceChange', 'currentBalance'])
+    curamt = initamt
+    tdlog = get_strategy()
+    tdlog = tdlog[tdlog['startDate'].astype(str)>startDate]
+    tdlog = tdlog[tdlog['endDate'].astype(str)<endDate]
     tdperiod = TradingDays(startDate=startDate, endDate=endDate)
+    tdperiod = [date.replace('-','') for date in tdperiod]
+
     for date in tdperiod:
-        flist = get_future_list()
+        daylog = tdlog[(tdlog.startDate.astype(str) == date) | (tdlog.endDate.astype(str) == date)]
+        if len(daylog.index) == 0:
+            continue
+        print(date)
+        buylog = daylog[daylog['startDate'].astype(str)==date]
+        buylog = buylog.sort_values('startTick')
+        buylog.reset_index(drop = True, inplace= True)
+
+        selllog = daylog[daylog['endDate'].astype(str) == date]
+        selllog = selllog.sort_values('endTick')
+        selllog.reset_index(drop = True, inplace = True)
+
+        i1 = i2 = 0
+        while i1 + i2 < len(buylog.index) + len(selllog.index):
+            while i1 < len(buylog.index) and i2 < len(selllog.index):
+                if buylog['startTick'].iloc[i1] < selllog['endTick'].iloc[i2]:
+
+                    if buylog['TradeMethod'].iloc[i1] == 'Long':
+                        if curamt < buylog['buyFuture'].iloc[i1]:
+                            tdlog = tdlog.drop(tdlog[(tdlog['startDate'] == buylog['startDate'].iloc[i1]) &
+                                                     (tdlog['startTick'] == buylog['startTick'].iloc[i1]) &
+                                                     (tdlog['FutureName'] == buylog['FutureName'].iloc[i1])].index,
+                                               axis=0)
+                            i1 += 1
+                            continue
+                        amtchange = -buylog['buyFuture'].iloc[i1]
+                        curamt += amtchange
+                    else:
+                        if curamt < buylog['buyETF'].iloc[i1]:
+                            tdlog = tdlog.drop(tdlog[(tdlog['startDate'] == buylog['startDate'].iloc[i1]) &
+                                                     (tdlog['startTick'] == buylog['startTick'].iloc[i1]) &
+                                                     (tdlog['FutureName'] == buylog['FutureName'].iloc[i1])].index,
+                                               axis=0)
+                            i1 += 1
+                            continue
+                        amtchange = -buylog['buyETF'].iloc[i1]
+                        curamt += amtchange
+
+                    balance = balance.append({'date': date, 'timetick': buylog['startTick'].iloc[i1],
+                                              'tradeMove': 'Buy '+buylog['FutureName'].iloc[i1], 'BalanceChange':
+                                                  amtchange, 'currentBalance': curamt}, ignore_index=True)
+                    i1 += 1
+                elif buylog['startTick'].iloc[i1] > selllog['endTick'].iloc[i2]:
+                    if selllog['TradeMethod'].iloc[i2] == 'Long':
+                        amtchange = (selllog['buyETF'].iloc[i2] - selllog['sellETF'].iloc[i2] +
+                                     selllog['sellFuture'].iloc[i2])
+                        curamt += amtchange
+                        balance = balance.append({'date': date, 'timetick': selllog['endTick'].iloc[i2],
+                                                  'tradeMove': 'Sell '+ selllog['FutureName'].iloc[i2], 'BalanceChange'
+                                                  : amtchange, 'currentBalance': curamt}, ignore_index=True)
+                    else:
+                        amtchange = (selllog['buyFuture'].iloc[i2] - selllog['sellFuture'].iloc[i2] +
+                                     selllog['sellETF'].iloc[i2])
+                        curamt += amtchange
+                        balance = balance.append({'date': date, 'timetick': selllog['endTick'].iloc[i2],
+                                                  'tradeMove': 'Sell ' + selllog['FutureName'].iloc[i2], 'BalanceChange'
+                                                  : amtchange, 'currentBalance': curamt}, ignore_index=True)
+                    i2 += 1
+                else:
+                    if selllog['TradeMethod'].iloc[i2] == 'Long':
+                        amtchange = (selllog['buyETF'].iloc[i2] - selllog['sellETF'].iloc[i2] +
+                                     selllog['sellFuture'].iloc[i2])
+                        curamt += amtchange
+                        balance = balance.append({'date': date, 'timetick': selllog['endTick'].iloc[i2],
+                                                  'tradeMove': 'Sell ' + selllog['FutureName'].iloc[i2], 'BalanceChange'
+                                                  : amtchange, 'currentBalance': curamt}, ignore_index=True)
+
+                    else:
+                        amtchange = (selllog['buyFuture'].iloc[i2] - selllog['sellFuture'].iloc[i2] +
+                                     selllog['sellETF'].iloc[i2])
+                        curamt += amtchange
+                        balance = balance.append({'date': date, 'timetick': selllog['endTick'].iloc[i2],
+                                                  'tradeMove': 'Sell ' + selllog['FutureName'].iloc[i2], 'BalanceChange'
+                                                  : amtchange, 'currentBalance': curamt}, ignore_index=True)
+                    i2 += 1
+
+
+                    if buylog['TradeMethod'].iloc[i1] == 'Long':
+                        if curamt < buylog['buyFuture'].iloc[i1]:
+                            tdlog = tdlog.drop(tdlog[(tdlog['startDate'] == buylog['startDate'].iloc[i1]) &
+                                                     (tdlog['startTick'] == buylog['startTick'].iloc[i1]) &
+                                                     (tdlog['FutureName'] == buylog['FutureName'].iloc[i1])].index,
+                                               axis=0)
+                            i1 += 1
+                            continue
+                        amtchange = -buylog['buyFuture'].iloc[i1]
+                        curamt += amtchange
+                    else:
+                        if curamt < buylog['buyETF'].iloc[i1]:
+                            tdlog = tdlog.drop(tdlog[(tdlog['startDate'] == buylog['startDate'].iloc[i1]) &
+                                                     (tdlog['startTick'] == buylog['startTick'].iloc[i1]) &
+                                                     (tdlog['FutureName'] == buylog['FutureName'].iloc[i1])].index,
+                                               axis=0)
+                            i1 += 1
+                            continue
+                        amtchange = -buylog['buyETF'].iloc[i1]
+                        curamt += amtchange
+
+                    balance = balance.append({'date': date, 'timetick': buylog['startTick'].iloc[i1],
+                                              'tradeMove': 'Buy ' + buylog['FutureName'].iloc[i1], 'BalanceChange':
+                                                  amtchange, 'currentBalance': curamt}, ignore_index=True)
+
+                    i1 += 1
+
+            while i1 < len(buylog.index) and i2 == len(selllog.index):
+
+                if buylog['TradeMethod'].iloc[i1] == 'Long':
+                    if curamt < buylog['buyFuture'].iloc[i1]:
+                        tdlog = tdlog.drop(tdlog[(tdlog['startDate'] == buylog['startDate'].iloc[i1]) &
+                                                 (tdlog['startTick'] == buylog['startTick'].iloc[i1]) &
+                                                 (tdlog['FutureName'] == buylog['FutureName'].iloc[i1])].index,
+                                           axis=0)
+                        i1 += 1
+                        continue
+                    amtchange = -buylog['buyFuture'].iloc[i1]
+                    curamt += amtchange
+                else:
+                    if curamt < buylog['buyETF'].iloc[i1]:
+                        tdlog = tdlog.drop(tdlog[(tdlog['startDate'] == buylog['startDate'].iloc[i1]) &
+                                                 (tdlog['startTick'] == buylog['startTick'].iloc[i1]) &
+                                                 (tdlog['FutureName'] == buylog['FutureName'].iloc[i1])].index,
+                                           axis=0)
+                        i1 += 1
+                        continue
+                    amtchange = -buylog['buyETF'].iloc[i1]
+                    curamt += amtchange
+
+                balance = balance.append({'date': date, 'timetick': buylog['startTick'].iloc[i1],
+                                          'tradeMove': 'Buy ' + buylog['FutureName'].iloc[i1], 'BalanceChange':
+                                              amtchange, 'currentBalance': curamt}, ignore_index=True)
+                i1 += 1
+
+            while i2 <len(selllog.index) and i1 == len(buylog.index):
+                if selllog['TradeMethod'].iloc[i2] == 'Long':
+                    amtchange = (selllog['buyETF'].iloc[i2] - selllog['sellETF'].iloc[i2] +
+                                 selllog['sellFuture'].iloc[i2])
+                    curamt += amtchange
+                    balance = balance.append({'date': date, 'timetick': selllog['endTick'].iloc[i2],
+                                              'tradeMove': 'Sell ' + selllog['FutureName'].iloc[i2], 'BalanceChange'
+                                              : amtchange, 'currentBalance': curamt}, ignore_index=True)
+
+                else:
+                    amtchange = (selllog['buyFuture'].iloc[i2] - selllog['sellFuture'].iloc[i2] +
+                                 selllog['sellETF'].iloc[i2])
+                    curamt += amtchange
+                    balance = balance.append({'date': date, 'timetick': selllog['endTick'].iloc[i2],
+                                              'tradeMove': 'Sell ' + selllog['FutureName'].iloc[i2], 'BalanceChange'
+                                              : amtchange, 'currentBalance': curamt}, ignore_index=True)
+                i2 += 1
+    balance.to_csv('./testbls20.csv')
     return
+
+def get_strategy():
+    tradelog = pd.read_csv('./tradelog/tradelog5modified.csv', index_col=0)
+    return tradelog
 
 def get_future_list(date):
     flist = fUtils.FuturesDailySummaryFrame('IF', date)['InstrumentId'].to_list() + \
@@ -19,11 +181,11 @@ def get_future_list(date):
 
     return flist
 
-def get_daily_trades(flist):
+def get_daily_trades(flist, balancedf):
     for future in flist:
 
         pass
     return
 
 
-get_sim_log('20200423','20200424',10000000)
+get_sim_log('20170101','20201231',10000000)
---
+++ rmTradeMethod.py
@@ -0,0 +1,301 @@
+from higgsboom.MarketData.CFuturesMarketDataUtils import *
+import pandas as pd
+import numpy as np
+from higgsboom.MarketData.CSecurityMarketDataUtils import *
+import datetime
+
+fUtils = CFuturesMarketDataUtils('Z:/FuturesData', 'cffex-l2')
+secUtils = CSecurityMarketDataUtils('Z:/StockData')
+fTradingDays = np.row_stack(fUtils.FuturesTradingDays('IF'))
+hTradingDays = np.row_stack(fUtils.FuturesTradingDays('IH'))
+
+tradelim = 3
+
+
+def get_df(future, date):
+    rtarr = pd.read_csv('./result/' + future[0:2] + '/' + future + '/' + date + '.csv')
+    return rtarr
+
+
+def get_short_trade_log(future, buyspread, sellspread):
+    curamt = 0
+    name = future
+    tradelist = get_trade_date(name)
+    tradelog = pd.DataFrame(
+        columns=['startDate', 'startTick', 'endDate', 'endTick', 'buyFuture', 'buyETF',
+                 'sellFuture', 'sellETF', 'returnRate', 'startError', 'endError'])
+
+    curhold = pd.DataFrame(columns=['startDate', 'initAmount', 'canbuy'])
+
+    for i in range(len(tradelist)):
+        tradelist[i] = tradelist[i].replace('-', '')
+    for date in tradelist:
+        print(date)
+        date = date.replace('-', '')
+        rtarr = get_df(future, date)
+        for index in range(len(curhold.index)):
+            if curhold['canbuy'].iloc[index] == False:
+                rtarr['sellRate'] = pd.to_numeric(rtarr['sellRate'])
+
+                rtarrs = rtarr[(rtarr['buy' + future[0:2]] - rtarr['sellETF'] / rtarr['IndexPrice']) < sellspread]
+                # rtarrs = rtarrs[(rtarrs['buy' + future[0:2]] - rtarrs['IndexPrice'] / rtarrs['IndexPrice']) < sellspread]
+                rtarrs = rtarrs[rtarrs['sellRate'] != 0]
+                print(date)
+                if date != tradelist[-1]:
+                    if len(rtarrs.index) == 0:
+                        print('cant sell case0')
+
+                    else:
+                        print('trying to sell, case1')
+                        i = len(tradelog.index) - 1
+                        tradelog.loc[i] = [tradelog['startDate'].iloc[i], tradelog['startTick'].iloc[i], date,
+                                           rtarrs['timetick'].iloc[0], tradelog['buyFuture'].iloc[i],
+                                           tradelog['buyETF'].iloc[i],
+                                           rtarrs['sell' + future[0:2]].iloc[0], rtarrs['buyETF'].iloc[0], 0,
+                                           tradelog['startError'].iloc[i],
+                                           rtarrs['IndexPrice'].iloc[0] - rtarrs['buyETF'].iloc[0]]
+                        canbuy = True
+                else:
+                    if len(rtarrs.index) == 0:
+                        rtarrs = rtarr[rtarr['timetick'] > '14:30:00.000']
+                        rtarrs = rtarrs[rtarrs['sellRate'] != 0]
+                        print('trying to sell, case2')
+                        i = len(tradelog.index) - 1
+                        tradelog.loc[i] = [tradelog['startDate'].iloc[i], tradelog['startTick'].iloc[i], date,
+                                           rtarrs['timetick'].iloc[0], tradelog['buyFuture'].iloc[i],
+                                           tradelog['buyETF'].iloc[i],
+                                           rtarrs['sell' + future[0:2]].iloc[0], rtarrs['buyETF'].iloc[0], 0,
+                                           tradelog['startError'].iloc[i],
+                                           rtarrs['IndexPrice'].iloc[0] - rtarrs['buyETF'].iloc[0]]
+
+                        canbuy = True
+                        continue
+                    print('trying to sell, case3')
+                    i = len(tradelog.index) - 1
+                    tradelog.loc[i] = [tradelog['startDate'].iloc[i], tradelog['startTick'].iloc[i], date,
+                                       rtarrs['timetick'].iloc[0], tradelog['buyFuture'].iloc[i],
+                                       tradelog['buyETF'].iloc[i],
+                                       rtarrs['buy' + future[0:2]].iloc[0], rtarrs['sellETF'].iloc[0], 0,
+                                       tradelog['startError'].iloc[i],
+                                       rtarrs['IndexPrice'].iloc[0] - rtarrs['sellETF'].iloc[0]]
+                    canbuy = True
+                    continue
+
+        if curhold['canbuy'].iloc[index] == True:
+            rtarr['buyRate'] = pd.to_numeric(rtarr['buyRate'])
+            rtarrb = rtarr[(rtarr['buy' + future[0:2]] - rtarr['sellETF']) / rtarr['IndexPrice'] > buyspread]
+            # rtarrb = rtarrb[(rtarrb['buy' + future[0:2]] - rtarrb['IndexPrice']) / rtarrb['IndexPrice'] > buyspread]
+            rtarrb = rtarrb[rtarrb['buyRate'] != 0]
+            if date != tradelist[-1]:
+                if len(rtarrb.index) == 0:
+                    print('cant buy, case 0')
+                    continue
+                else:
+                    print('trying to buy, case 1')
+                    canbuy = False
+                    tradelog.loc[len(tradelog.index)] = [date, rtarrb['timetick'].iloc[0], 'TBD', 'TBD',
+                                                         rtarrb['buy' + future[0:2]].iloc[0],
+                                                         rtarrb['sellETF'].iloc[0], 'TBD', 'TBD', 0,
+                                                         rtarrb['IndexPrice'].iloc[0] - rtarrb['buyETF'].iloc[0], 'TBD']
+                    continue
+            # cant buy cuz its last day
+            print('cant buy, case 2')
+            continue
+
+        curhold.reset_index(drop=True, inplace=True)
+
+    print(tradelog)
+    list = ['buyFuture', 'buyETF', 'sellFuture', 'sellETF']
+    for i in list:
+        tradelog[i] = pd.to_numeric(tradelog[i])
+    tradelog['returnRate'] = (tradelog['sellETF'] - tradelog['buyETF'] - tradelog['sellFuture'] + tradelog[
+        'buyFuture']) / (
+                                 tradelog['sellETF'])
+    print(tradelog)
+
+    return tradelog
+
+
+def get_long_trade_log(future, buyspread, sellspread):
+    name = future
+    tradelist = get_trade_date(name)
+    tradelog = pd.DataFrame(
+        columns=['startDate', 'startTick', 'endDate', 'endTick', 'buyFuture', 'buyETF',
+                 'sellFuture', 'sellETF', 'returnRate', 'startError', 'endError'])
+    canbuy = True
+    for i in range(len(tradelist)):
+        tradelist[i] = tradelist[i].replace('-', '')
+    for date in tradelist:
+        print(date)
+        date = date.replace('-', '')
+        rtarr = get_df(future, date)
+        if canbuy == False:
+            rtarr['buyRate'] = pd.to_numeric(rtarr['buyRate'])
+            rtarrs = rtarr[(rtarr['sellETF'] - rtarr['buy' + future[0:2]]) / rtarr['IndexPrice'] < sellspread]
+            # rtarrs = rtarrs[(rtarrs['IndexPrice'] - rtarrs['buy' + future[0:2]]) / rtarrs['IndexPrice'] < sellspread]
+            rtarrs = rtarrs[rtarrs['buyRate'] != 0]
+            print(date)
+            if date != tradelist[-1]:
+                if len(rtarrs.index) == 0:
+                    print('cant sell case0')
+
+                else:
+                    print('trying to sell, case1')
+                    i = len(tradelog.index) - 1
+                    tradelog.loc[i] = [tradelog['startDate'].iloc[i], tradelog['startTick'].iloc[i], date,
+                                       rtarrs['timetick'].iloc[0], tradelog['buyFuture'].iloc[i],
+                                       tradelog['buyETF'].iloc[i],
+                                       rtarrs['sell' + future[0:2]].iloc[0], rtarrs['buyETF'].iloc[0], 0,
+                                       tradelog['startError'].iloc[i],
+                                       rtarrs['IndexPrice'].iloc[0] - rtarrs['buyETF'].iloc[0]]
+                    canbuy = True
+            else:
+                if len(rtarrs.index) == 0:
+                    rtarrs = rtarr[rtarr['timetick'] > '14:30:00.000']
+                    rtarrs = rtarrs[rtarrs['buyRate'] != 0]
+                    print('trying to sell, case2')
+                    i = len(tradelog.index) - 1
+                    tradelog.loc[i] = [tradelog['startDate'].iloc[i], tradelog['startTick'].iloc[i], date,
+                                       rtarrs['timetick'].iloc[0], tradelog['buyFuture'].iloc[i],
+                                       tradelog['buyETF'].iloc[i],
+                                       rtarrs['sell' + future[0:2]].iloc[0], rtarrs['buyETF'].iloc[0], 0,
+                                       tradelog['startError'].iloc[i],
+                                       rtarrs['IndexPrice'].iloc[0] - rtarrs['buyETF'].iloc[0]]
+                    canbuy = True
+                    continue
+                print('trying to sell, case3')
+                i = len(tradelog.index) - 1
+                tradelog.loc[i] = [tradelog['startDate'].iloc[i], tradelog['startTick'].iloc[i], date,
+                                   rtarrs['timetick'].iloc[0], tradelog['buyFuture'].iloc[i],
+                                   tradelog['buyETF'].iloc[i],
+                                   rtarrs['buy' + future[0:2]].iloc[0], rtarrs['sellETF'].iloc[0], 0,
+                                   tradelog['startError'].iloc[i],
+                                   rtarrs['IndexPrice'].iloc[0] - rtarrs['buyETF'].iloc[0]]
+                canbuy = True
+                continue
+
+        if canbuy == True:
+
+            rtarr['buyRate'] = pd.to_numeric(rtarr['buyRate'])
+            rtarrb = rtarr[(rtarr['sellETF'] - rtarr['buy' + future[0:2]]) / rtarr['IndexPrice'] > buyspread]
+            # rtarrb = rtarrb[(rtarrb['IndexPrice'] - rtarrb['buy' + future[0:2]]) / rtarrb['IndexPrice'] > buyspread]
+            rtarrb = rtarrb[rtarrb['sellRate'] != 0]
+            if date != tradelist[-1]:
+                if len(rtarrb.index) == 0:
+                    print('cant buy, case 0')
+                    continue
+                else:
+                    print('trying to buy, case 1')
+                    canbuy = False
+                    tradelog.loc[len(tradelog.index)] = [date, rtarrb['timetick'].iloc[0], 'TBD', 'TBD',
+                                                         rtarrb['buy' + future[0:2]].iloc[0],
+                                                         rtarrb['sellETF'].iloc[0], 'TBD', 'TBD', 0,
+                                                         rtarrb['IndexPrice'].iloc[0] - rtarrb['sellETF'].iloc[0], 'TBD'
+                                                         ]
+                    continue
+            # cant buy cuz its last day
+            print('cant buy, case 2')
+            continue
+
+    print(tradelog)
+    list = ['buyFuture', 'buyETF', 'sellFuture', 'sellETF']
+    for i in list:
+        tradelog[i] = pd.to_numeric(tradelog[i])
+    tradelog['returnRate'] = (-tradelog['sellETF'] + tradelog['buyETF'] + tradelog['sellFuture'] - tradelog[
+        'buyFuture']) / (
+                                 tradelog['sellETF'])
+    print(tradelog)
+    return tradelog
+
+
+def get_trade_date(name):
+    year = name[2:4]
+    month = name[4:]
+
+    # get the month and year of start date
+    if int(month) == 3 or int(month) == 6 or int(month) == 9 or int(month) == 12:
+        startmonth = int(month) - 7
+        if startmonth > 0:
+            startmonth = str(startmonth)
+            startyear = year
+        else:
+            startmonth = str(12 + startmonth)
+            startyear = str(int(year) - 1)
+    else:
+        startmonth = int(month) - 2
+        if startmonth > 0:
+            startmonth = str(startmonth)
+            startyear = year
+        else:
+            startmonth = str(12 + startmonth)
+            startyear = str(int(year) - 1)
+
+    # get the start date
+    for i in range(0, 7):
+        startday = str(15 + i)
+        if datetime.datetime.strptime('20' + startyear + startmonth + startday, '%Y%m%d').weekday() == 4:
+            startday = str(int(startday))
+            startdate = (datetime.datetime.strptime('20' + startyear + startmonth + startday,
+                                                    '%Y%m%d') + datetime.timedelta(days=3)).date()
+            break
+
+    # get the end date
+    for i in range(0, 7):
+        endday = str(15 + i)
+        if datetime.datetime.strptime('20' + year + month + endday, '%Y%m%d').weekday() == 4:
+            enddate = (datetime.datetime.strptime('20' + year + month + endday, '%Y%m%d').date())
+            break
+
+    startdate = str(startdate).replace('-', '')
+    enddate = str(enddate).replace('-', '')
+
+    if name[0:2] == 'IH':
+        tradelist = hTradingDays[hTradingDays[:, 0] > startdate]
+        tradelist = tradelist[tradelist[:, 0] <= enddate]
+    if name[0:2] == 'IF':
+        tradelist = fTradingDays[fTradingDays[:, 0] > startdate]
+        tradelist = tradelist[tradelist[:, 0] <= enddate]
+
+    tradelist = list(tradelist[:, 0])
+    for tdday in tradelist:
+        tradingfuture = fUtils.FuturesList(name[0:2], tdday)
+        try:
+            b = tradingfuture.index(name)
+        except ValueError:
+            tradelist.remove(tdday)
+        else:
+            continue
+
+    return tradelist
+
+
+if __name__ == '__main__':
+    list1 = ['IH18', 'IF18', 'IH19', 'IF19', 'IH20', 'IF20']
+    list2 = range(1, 13)
+    buyspread = 0.015
+    sellspread = 0.005
+    for i1 in list1:
+        for i2 in list2:
+            if i2 in [3, 6, 9, 12]:
+                continue
+            if i2 < 10:
+                name = str(i1) + '0' + str(i2)
+            else:
+                name = str(i1) + str(i2)
+            tradeloglong = get_long_trade_log(name, buyspread, sellspread)
+            tradelogshort = get_short_trade_log(name, buyspread, sellspread)
+
+            pathlong = './result/' + name[0:2] + '/rmlongFuture1/'
+            pathshort = './result/' + name[0:2] + '/rmshortFuture1/'
+
+            folderlong = os.path.exists(pathlong)
+            foldershort = os.path.exists(pathshort)
+            if not folderlong:
+                os.makedirs(pathlong)
+            if not foldershort:
+                os.makedirs(pathshort)
+
+            tradeloglong.to_csv(pathlong + name + '.csv', index=False)
+            tradelogshort.to_csv(pathshort + name + '.csv', index=False)
+
+            print('logged' + name)
